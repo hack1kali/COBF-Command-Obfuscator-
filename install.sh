49dc257a9d775de94e959c855df81c6949cbc587
DeenFlow <166843765+hack1kali@users.noreply.github.com>
2026-04-29 21:24:56 +0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "[+] Installing cobf Obfuscator..."
 chmod +x cobf.py
-sudo cp cobf.py /usr/local/bin/deenflow
+sudo cp cobf.py /usr/local/bin/cobf
 echo "[+] Done! You can now run the tool by typing 'deenflow' in your terminal."
